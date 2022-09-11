d5a3f41cf30dbd56694debed137e543842e90671
Lídia Campos <44880050+llidiaccampos@users.noreply.github.com>
2022-09-11 23:41:45 +0000

    Questão 09 finalizada (falta tratar os anos bissextos para validar a data de nascimento)

--- 01_Lista_Função/q09.c
+++ 01_Lista_Função/q09.c
@@ -14,92 +14,105 @@ masculino; f e F para feminino, o e O para outro).
 #include <math.h>
 
 typedef struct{
-  char nome [21];
+  char nome [30];
   int dataNasc_dia;
   int dataNasc_mes;
   int dataNasc_ano;
-  long cpf;
+  char cpf [20];
   char sexo; 
 } dados_cliente;
 
-dados_cliente cadastrar_cliente (dados_cliente dc);
+typedef struct{
+  int geral;
+  int nome;
+  int sexo;
+  int v_cpf;
+  int data_nasc; 
+} valid_cadastro;
 
+valid_cadastro cadastrar_cliente ();
 int validarNome (char n[]);
-
 int validarSexo (char sx); 
-
-int validarCPF (long cpf);
-
-/*--------------------------
+int validarCPF (char cpf_int[]);
+int validarNacimento (int dia, int mes, int ano);
 
 int main(void) {
-   
- dados_cliente cliente;
-  int icont;
-    
-  cliente= cadastrar_cliente(cliente);
-
-printf("\nNome: %s - Data: %d/%d/%d\nCPF: %ld - Sexo: %s", cliente.nome, cliente.dataNasc_dia, cliente.dataNasc_mes, cliente.dataNasc_ano, cliente.cpf, cliente.sexo);
   
-            
+  valid_cadastro validador;
+  
+  validador= cadastrar_cliente();
+
+  if (validador.geral==4)
+    printf(" Cadastro realizado com sucesso");
+  
+  else{
+    printf("Cadastro não finalizado\n");
+    
+    if(validador.nome==0)
+      printf("Falha no cadastro do nome\n");
+    if(validador.sexo==0)
+      printf("Falha no cadastro do sexo\n");
+    if(validador.v_cpf==0)
+      printf("Falha no cadastro do cpf\n");
+    if(validador.data_nasc==0)
+      printf("Falha no cadastro da data de nascimento");
+  }
+         
   return 0;
 }
 
-dados_cliente cadastrar_cliente (dados_cliente dc){
+//FUNÇÕES
+
+valid_cadastro cadastrar_cliente (){
 
   dados_cliente dados;
-  int len, i;
-  char sexo_cliente;
-
+  valid_cadastro cadastro;
+  int len1, len2, valid_nome, valid_sx, valid_cpf, valid_nasc;
+    
   printf("Informe o nome do cliente: ");
   fgets(dados.nome, sizeof(dados.nome), stdin);
-  len = strlen(dados.nome);
-  if (dados.nome[len - 1] == '\n')
-    dados.nome[len - 1] = '\0';
-
+  len1 = strlen(dados.nome);
+  if (dados.nome[len1 - 1] == '\n')
+    dados.nome[len1 - 1] = '\0';
+  
+  printf("Informe o sexo do cliente (F: Feminino/ M: Masculino / O: outro): ");
+  scanf("%c", & dados.sexo);
+  getchar();
+    
   printf("Informe o dia do nascimento do cliente: ");
   scanf ("%d", & dados.dataNasc_dia);
-
+    
   printf("Informe o mês do nascimento do cliente: ");
   scanf ("%d", & dados.dataNasc_mes);
-
+    
   printf("Informe o ano do nascimento do cliente: ");
   scanf ("%d", & dados.dataNasc_ano);
 
-  printf("Informe o CPF do cliente: ");
-  scanf ("%ld", & dados.cpf);
-
-  __fpurge (stdin); 
+  //CORRIGIR PROBLEMA DE BUFFER
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF) { }
+  //
   
-  printf("Informe o sexo do cliente (F: Feminino/ M: Masculino / O: outro): ");
-  scanf("%c", & sexo_cliente);
-  getchar();
-    
-  if (sexo_cliente== 'F'|| sexo_cliente== 'f'){
-    char sexxo []= "Feminino";
-    for (i=0; sexxo[i] != '\0'; i++)
-     dados.sexo[i]=sexxo[i];
-    dados.sexo[i]='\0';
-  }
+  printf("Informe o CPF do cliente: ");
+  fgets(dados.cpf, sizeof(dados.cpf), stdin);
+  len2 = strlen(dados.cpf);
+  if (dados.cpf[len2 - 1] == '\n')
+    dados.cpf[len2 - 1] = '\0';
+     
+  valid_nome= validarNome (dados.nome);
+  valid_sx= validarSexo (dados.sexo); 
+  valid_cpf= validarCPF (dados.cpf);
+  valid_nasc= validarNacimento (dados.dataNasc_dia, dados.dataNasc_mes, dados.dataNasc_ano);
 
-  if (sexo_cliente== 'M'|| sexo_cliente== 'm'){
-    char sexxo []= "Masculino";
-    for (i=0; sexxo[i] != '\0'; i++)
-     dados.sexo[i]=sexxo[i];
-    dados.sexo[i]='\0';
-  }
-
-  if (sexo_cliente== 'O'|| sexo_cliente== 'o'){
-    char sexxo []= "Outro";
-    for (i=0; sexxo[i] != '\0'; i++)
-     dados.sexo[i]=sexxo[i];
-    dados.sexo[i]='\0';
-  }
-            
-  return dados; 
+  cadastro.geral= valid_nome + valid_sx + valid_cpf + valid_nasc;
+  cadastro.nome= valid_nome;
+  cadastro.sexo= valid_sx;
+  cadastro.v_cpf= valid_cpf;
+  cadastro.data_nasc= valid_nasc;
+  
+  return cadastro; 
 }
 
---------------------------*/
 
 int validarNome (char n[]){
 
@@ -135,36 +148,99 @@ int validarSexo (char sx){
 }  
 }
 
-int validarCPF (long cpf){
+int validarCPF (char cpf_int[]){
+
+  int cpf [11], validador, k, t, invalid;
+  int i, dg1, soma_dg1=0, valid_dg1, verific_dg1=0; 
+  int j, dg2, soma_dg2=0, valid_dg2, verific_dg2=0;
+
+  if (strlen (cpf_int) != 11)
+    return validador= 0;
   
-  int validador1= 0, validador2, soma=0, e=10; 
-  long val_digito1= cpf, val_digito2=cpf;
-  
-  if (cpf< pow (10, 11)){
+  else{
+
+    for (t=0; t<=10; t++)
+      cpf[t]= cpf_int[t] - '0';
+            
+    for (i=0, dg1= 10; i<9; i++){
+    soma_dg1= soma_dg1 + (cpf[i] * dg1);
+    dg1--;
+    }
+
+    valid_dg1= (soma_dg1*10)%11;
     
-    for (e= 10; e>2; e--){
-      
-      x= val_digito1/(pow 10, e);
-      if (val_digito1 > (pow 10, e)){
-        val_digito1= (val_digito1 - (x * (pow 10, e)))
-        soma= soma + (x * e);
-      }
-        
-      else
-        val_digito1= val_digito1%(pow 10, e);
-        e--;
-        
+    if (valid_dg1== cpf[9] || (valid_dg1== 10 && cpf[9]== 0))
+      verific_dg1= 1;
+    
+    for (j=0, dg2= 11; j<10; j++){
+      soma_dg2= soma_dg2 + (cpf[j] * dg2);
+      dg2--;
     }
   
+    valid_dg2= (soma_dg2*10)%11;
+    
+    if (valid_dg2== cpf[10] || (valid_dg2== 10 && cpf[10]== 0))
+      verific_dg2= 1;
+  
+    for (k=0, invalid= 0; k<10; k++){
+      if (cpf[k]==cpf[k+1])
+        invalid++; 
+    }
+  
+    if (verific_dg1== 1 && verific_dg2== 1 && invalid!=10)
+      return validador=1;
+  
+    else
+      return validador=0;    
+    }
+     
+}
+
+int validarNacimento (int dia, int mes, int ano){
+
+  int validador, valid_ano=0, valid_mes=0, valid_dia=0;
+  int mes_31=0, mes_30=0;
+  
+  if (ano >= 1 && ano <= 2022)
+    valid_ano= 1;
+
+  if (mes >= 1 && mes <= 12)
+    valid_mes= 1;
+
+  if (valid_mes==1 && dia>=1 && dia<= 31){
+     
+    switch (mes) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      mes_31= 1;
+    break;
+//Tratanto fevereiro como mês de 30 dias. 
+//Falta fazer função para verifcar ano bissexto.
+    case 2: 
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      mes_30= 1;
+    break;
+    }  
+    
+  if (dia<=31 && mes_31==1)
+    valid_dia= 1;
+
+  if (dia<=30 && mes_30==1)
+    valid_dia= 1; 
   }
 
-    x= x/10;
-    
-    
-    
-  }
+  if (valid_ano + valid_mes+ valid_dia==3)
+    return validador=1;
 
   else
-    return validador2=0
-      
+    return validador=0;
+  
 }
